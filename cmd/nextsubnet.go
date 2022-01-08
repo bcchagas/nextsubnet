b0c37111542056300b326a802429be5230d51e2e
Bruno Chagas <bcchagas@gmail.com>
2022-01-08 15:57:04 -0300

    Added .vscode/lauch.json for debbuging. Changed the flags to reference a preexisting struct (so I dont have to lookup the value, one step less)

--- cmd/nextsubnet.go
+++ cmd/nextsubnet.go
@@ -14,17 +14,24 @@ import (
 var nextsubnetCmd = &cobra.Command{
 	Use:   "nextsubnet -n network -m mask [--ignore-list list | --ignore-file file]",
 	Short: "Find the next subnet available for a network",
-	Long: `Find the next subnet available for a network.
-
-Examples:
-	# Find the next /24 subnet in the network 10.0.0.1/22 that does not overlaps the two subnets existent
-	bccli nextsubnet --network "10.0.0.1/22" --mask 24 --ignore-list "10.0.0.1/24,10.0.0.2/25"
-
-	# You can also pass in a file containing the subnets in use
-	bccli nextsubnet --network "10.0.0.1/22"  --mask 24 --ignore-file subnets.txt`,
+	Long:  `Find the next subnet available for a network.`,
+	Example: "  # Find the next /24 subnet in the network 10.0.0.1/22 that doesn't overlap any of the two existent subnets\n" +
+		"  bccli nextsubnet --network 10.0.0.1/22 --mask 24 --ignore-list \"10.0.0.1/24,10.0.0.2/25\"\n\n" +
+		"  # You can also pass in a file containing the subnets in use\n" +
+		"  bccli nextsubnet --network 10.0.0.1/22  --mask 24 --ignore-file subnets.txt\n",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello")
+		fmt.Println(nextsubnetFlags.network)
+		fmt.Println(nextsubnetFlags.mask)
+		fmt.Println(nextsubnetFlags.ignoreList)
+		fmt.Println(nextsubnetFlags.IgnoreFile)
 	},
+}
+
+var nextsubnetFlags struct {
+	network    string
+	mask       int
+	ignoreList string
+	IgnoreFile string
 }
 
 func init() {
@@ -38,13 +45,13 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	nextsubnetCmd.Flags().StringP("network", "n", "", "(Required) Address of the network the subnet will be based of in CIDR notation e.g. 10.0.0.0/24")
+	nextsubnetCmd.Flags().StringVarP(&nextsubnetFlags.network, "network", "n", "", "(Required) Address of the network the subnet will be based of in CIDR notation e.g. 10.0.0.0/24")
 	nextsubnetCmd.MarkFlagRequired("network")
 
-	nextsubnetCmd.Flags().IntP("mask", "m", 0, "(Required) Mask of the subnet to be found e.g. 24 for a '/24' subnet")
+	nextsubnetCmd.Flags().IntVarP(&nextsubnetFlags.mask, "mask", "m", 0, "(Required) Mask of the subnet to be found e.g. 24 for a '/24' subnet")
 	nextsubnetCmd.MarkFlagRequired("mask")
 
-	nextsubnetCmd.Flags().String("ignore-list", "", "List of subnets in CIDR notation separated by comma e.g. '10.0.0.0/24,10.0.0.1/24'")
-	nextsubnetCmd.Flags().String("ignore-file", "", "File containing the list of subnets in CIDR notation separated by line")
+	nextsubnetCmd.Flags().StringVar(&nextsubnetFlags.ignoreList, "ignore-list", "", "List of subnets in CIDR notation separated by comma e.g. '10.0.0.0/24,10.0.0.1/24'")
+	nextsubnetCmd.Flags().StringVar(&nextsubnetFlags.IgnoreFile, "ignore-file", "", "File containing the list of subnets in CIDR notation separated by line")
 
 }

ca37a89600ecf576ed4213181baa2de158fb0ec4
Bruno Chagas <bcchagas@gmail.com>
2022-01-09 19:48:46 -0300

    MVP

--- cmd/nextsubnet.go
+++ cmd/nextsubnet.go
@@ -5,8 +5,15 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math"
+	"net"
+	"os"
+	"strings"
 
+	"github.com/apparentlymart/go-cidr/cidr"
 	"github.com/spf13/cobra"
 )
 
@@ -19,39 +26,139 @@ var nextsubnetCmd = &cobra.Command{
 		"  bccli nextsubnet --network 10.0.0.1/22 --mask 24 --ignore-list \"10.0.0.1/24,10.0.0.2/25\"\n\n" +
 		"  # You can also pass in a file containing the subnets in use\n" +
 		"  bccli nextsubnet --network 10.0.0.1/22  --mask 24 --ignore-file subnets.txt\n",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(nextsubnetFlags.network)
-		fmt.Println(nextsubnetFlags.mask)
-		fmt.Println(nextsubnetFlags.ignoreList)
-		fmt.Println(nextsubnetFlags.IgnoreFile)
+	Args: cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if flags.ignoreList != "" && flags.ignoreFile != "" {
+			return fmt.Errorf("--ignore-list and --ignore-file specified")
+		}
+
+		// check mask is lower than the network block
+		if netMaskSize, _ := flags.network.Mask.Size(); netMaskSize >= flags.mask {
+			return fmt.Errorf("--mask %v must be greater than --network %v", flags.mask, flags.network.String())
+		}
+
+		subnetsInUse := make([]*net.IPNet, 0)
+		var err error
+
+		// TODO return the first in case ignore-file and ignore-list is not provided
+		// TODO read from ignore-file and process it
+		if flags.ignoreFile != "" {
+			subnetsInUse, err = ignoreFileParse(flags.ignoreFile)
+			if err != nil {
+				return err
+			}
+		}
+
+		// TODO process ignore-list
+		if flags.ignoreList != "" {
+			subnetsInUse, err = ignoreListParse(flags.ignoreList)
+			if err != nil {
+				return err
+			}
+		}
+
+		// TODO generate possible net values
+		netMaskSize, _ := flags.network.Mask.Size()
+		maskDiff := flags.mask - netMaskSize //flags.mask is not intuitive that it represents the subnets
+		subnetCapacity := math.Pow(2, float64(maskDiff))
+
+		// maybe the size wont work for ipv6 (or ipv4 with lower networks)
+		subnetCandidates := make([]*net.IPNet, int(subnetCapacity))
+		for i := 0; i < len(subnetCandidates); i++ {
+			fmt.Println(i)
+
+			subnetCandidates[i], err = cidr.Subnet(&flags.network, maskDiff, i)
+			if err != nil {
+				return err
+			}
+
+			// TODO when subnetsInUse contains two values that overlaps or a value that is not
+			// in the range of the network it will run for
+			// all subnetCandidates regardless and return a erro for each one of them. Better to
+			// fail fast before
+			err = cidr.VerifyNoOverlap(append(subnetsInUse, subnetCandidates[i]), &flags.network)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			// Every candidate is presummably to return an error until a subtible subnet is found
+			// When that is not the case, the flow will reach this point and break out of the loop
+			// with the nextsubnet
+			nextsubnet := subnetCandidates[i]
+			fmt.Println(nextsubnet)
+			break
+
+		}
+		return nil
 	},
 }
 
-var nextsubnetFlags struct {
-	network    string
+var flags struct {
+	network    net.IPNet
 	mask       int
 	ignoreList string
-	IgnoreFile string
+	ignoreFile string
 }
 
 func init() {
+
 	rootCmd.AddCommand(nextsubnetCmd)
 
-	// Here you will define your flags and configuration settings.
+	nextsubnetCmd.Flags().IPNetVarP(&flags.network, "network", "n", net.IPNet{},
+		"(Required) Address of the network the subnet will be based of in CIDR notation e.g. 10.0.0.0/22")
+	nextsubnetCmd.Flags().IntVarP(&flags.mask, "mask", "m", 0,
+		"(Required) Mask size of the subnet to be found e.g. 24")
+	nextsubnetCmd.Flags().StringVar(&flags.ignoreList, "ignore-list", "",
+		"List of subnets in CIDR notation separated by comma e.g. '10.0.0.0/24,10.0.0.1/24'")
+	nextsubnetCmd.Flags().StringVar(&flags.ignoreFile, "ignore-file", "",
+		"File containing the list of subnets in CIDR notation separated by line")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// nextsubnetCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	nextsubnetCmd.Flags().StringVarP(&nextsubnetFlags.network, "network", "n", "", "(Required) Address of the network the subnet will be based of in CIDR notation e.g. 10.0.0.0/24")
+	nextsubnetCmd.MarkFlagRequired("mask")
 	nextsubnetCmd.MarkFlagRequired("network")
 
-	nextsubnetCmd.Flags().IntVarP(&nextsubnetFlags.mask, "mask", "m", 0, "(Required) Mask of the subnet to be found e.g. 24 for a '/24' subnet")
-	nextsubnetCmd.MarkFlagRequired("mask")
+}
 
-	nextsubnetCmd.Flags().StringVar(&nextsubnetFlags.ignoreList, "ignore-list", "", "List of subnets in CIDR notation separated by comma e.g. '10.0.0.0/24,10.0.0.1/24'")
-	nextsubnetCmd.Flags().StringVar(&nextsubnetFlags.IgnoreFile, "ignore-file", "", "File containing the list of subnets in CIDR notation separated by line")
+// ignoreListParse receives a comma separated list of subnets and
+// returns a slice of net.IPNet
+func ignoreListParse(ignoreList string) ([]*net.IPNet, error) {
+	var tmpIPNetSlice []*net.IPNet
+	sliceOfStrings := strings.Split(ignoreList, ",")
+	for _, v := range sliceOfStrings {
+		_, tmpIPNet, err := net.ParseCIDR(v)
+		if err != nil {
+			return nil, err
+		}
+		tmpIPNetSlice = append(tmpIPNetSlice, tmpIPNet)
+	}
+	return tmpIPNetSlice, nil
+}
 
+// ignoreFileParse receives a path of file containing
+// a list of subnets in CIDR format and returns a slice
+// containing *net.IPNet
+func ignoreFileParse(ignoreFile string) ([]*net.IPNet, error) {
+	f, err := os.Open(flags.ignoreFile)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	tmpIPNetSlice := make([]*net.IPNet, 0)
+	for scanner.Scan() {
+		tmpString := strings.TrimSpace(scanner.Text())
+		// Ignore empty lines in the file
+		if len(tmpString) == 0 {
+			continue
+		}
+		_, tmpIPNet, err := net.ParseCIDR(tmpString)
+		if err != nil {
+			return nil, err
+		}
+		tmpIPNetSlice = append(tmpIPNetSlice, tmpIPNet)
+	}
+	return tmpIPNetSlice, nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module bccli
 go 1.17
 
 require (
+	github.com/apparentlymart/go-cidr v1.1.0
+	github.com/spf13/cobra v1.3.0
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/spf13/cobra v1.3.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
